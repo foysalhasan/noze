24d933f6ac8c6b02f60953dc493ac4324dc517eb
Foysal Hasan <foysalhasan499@gmail.com>
2022-07-20 06:15:32 +0600

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#654a9b',
+        dark: '#210545',
+      },
+    },
+  },
+  plugins: [],
+}
